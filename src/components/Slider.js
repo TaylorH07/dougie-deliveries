bca10aeff307a059b697075e8103fd8ee880e3b0
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-04-22 00:25:08 -0500

    added mainpage photo

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import {HiOutlineArrowCircleLeft, HiOutlineArrowCircleRight} from 'react-icons/hi'
-
+import LazyDog from '../images/lazy pug.jpeg'
+import Spainal from '../images/Spainal.jpeg'
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: lightseagreen;
+    background-color: white;
     position: relative;
 `
 const Arrow = styled.div`
@@ -26,6 +27,44 @@ const Arrow = styled.div`
     cursor: pointer;
     opacity: 0.8;
 `
+const Wrapper = styled.div`
+    height: 100%;
+`
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+const Image = styled.img`
+    height: 54.8%;
+`
+const Image2 =styled.img`
+    height: 50%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1 `
+font-size: 70px;
+`
+const Desc = styled.p `
+    margin: 50px 0px;
+    font-size: 20;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button `
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
 
 function Slider() {
   return (
@@ -33,6 +72,20 @@ function Slider() {
         <Arrow direction = "left">
            <HiOutlineArrowCircleLeft size='2.2em' /> 
         </Arrow>
+        <Wrapper>
+            <Slide>
+            
+            <ImgContainer>
+                <Image src={LazyDog} />
+                <Image2 src={Spainal} />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Summer Sale</Title>
+                <Desc>Get 20% off on all treats this month!</Desc>
+                <Button>Shop Now</Button>
+            </InfoContainer>
+            </Slide>
+        </Wrapper>
         <Arrow direction= "right">
            <HiOutlineArrowCircleRight size='2.2em'/> 
         </Arrow>
