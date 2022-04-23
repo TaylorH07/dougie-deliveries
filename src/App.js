03be436b419c4c205a90035eb6862c2883600bd4
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-04-23 13:42:01 -0500

    login/register page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import SignUp from './pages/Signup';
+import Login from './pages/Login';
 
 
 function App() {
   return (
-    <Product />
+    <Login />
   )
 }
 
