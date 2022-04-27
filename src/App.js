3d50f36cb291f78cf3caee050f4f4e0f837da8d7
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-04-26 19:35:07 -0500

    file change

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
-import Cart from './pages/Cart';
+import Cart from './pages/Detail';
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom'
+import { HiHome } from 'react-icons/hi';
 
 
 function App() {
   return (
-    <Home />
+    <Home/>
   )
 }
 
