a65da9fd35f20010e3c84f5a6b1c03257bc9eb70
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-04-27 19:12:20 -0500

    index.js adjust

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import Product from './pages/Product';
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import {BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 
 
 function App() {
   return (
-    <Home />
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products/:category" component={ProductList} />
+        <Route exact path="/product/:id" component={Product} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+
+          </Switch>
+      </Router>
+    </ApolloProvider>
   )
 }
 
